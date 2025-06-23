0e61693471f1a3cf58634b67faa8fe9f79fce9d1
Ran Arino <ranarino0611ca@gmail.com>
2025-06-23 16:06:01 +0900

    feat(api): include department management router in API v1

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .auth import router as auth_router
 from .users import router as user_router
 from .roles import router as role_router
+from .departments import router as department_router
 
 from .goals import router as goal_router
 from .evaluations import router as evaluation_router
@@ -16,6 +17,8 @@ api_router.include_router(auth_router)
 api_router.include_router(user_router)
 api_router.include_router(role_router)
 api_router.include_router(goal_router)
+api_router.include_router(department_router)
+
 api_router.include_router(evaluation_router)
 # api_router.include_router(reports_router)
 
