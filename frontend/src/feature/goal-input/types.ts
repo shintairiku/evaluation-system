95335de1e3ef1800b4e2b4772dda9760377b4d6a
Ran Arino <ranarino0611ca@gmail.com>
2026-01-06 17:57:07 +0900

    feat(goal-input): add default achievement criteria examples for goal evaluation

--- frontend/src/feature/goal-input/types.ts
+++ frontend/src/feature/goal-input/types.ts
@@ -10,3 +10,12 @@ export const DEFAULT_STAGE_WEIGHT_BUDGET: StageWeightBudget = {
   qualitative: 30,
   competency: 10,
 };
+
+export const DEFAULT_ACHIEVEMENT_CRITERIA_EXAMPLE = [
+  'SS：上位3%以内に入っている',
+  'S：上位10%以内に入っている',
+  'A：上位20%以内に入っている',
+  'B：上位40%以内に入っている',
+  'C：上位60%以内に入っている',
+  'D：上位60%未満',
+].join('\n');
