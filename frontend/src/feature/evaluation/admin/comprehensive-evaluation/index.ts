079b0c054871ca8c3bad938efdc8917e3e339c65
Ran Arino <ranarino0611ca@gmail.com>
2026-01-28 16:31:12 +0900

    feat: export Comprehensive Evaluation pages for admin section

---
+++ frontend/src/feature/evaluation/admin/comprehensive-evaluation/index.ts
@@ -0,0 +1,2 @@
+export { default as ComprehensiveEvaluationPage } from "./display/ComprehensiveEvaluationPage";
+export { default as ComprehensiveEvaluationCandidatesPage } from "./display/ComprehensiveEvaluationCandidatesPage";
